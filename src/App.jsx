c796c84954f40840cc2d2c0ce89e1363bfae669c
Omkar Hawaldar <o.hawaldar@bm-systems.in>
2025-07-09 17:48:03 +0530

    usedebounce hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useFetch } from "./hooks/useFetch";
 import { usePrev } from "./hooks/usePrev";
+import { useDebounce } from "./hooks/useDebounce";
 
 
 function App(){
     const {response , loading} = useFetch("https://jsonplaceholder.typicode.com/posts");
     const [count ,setCount] = useState(0);
+    const [inputValue , setInputValue]  =useState("");
+    const debouncedValue = useDebounce(inputValue,300);
+    useEffect(()=>{
+        console.log("Hellow there ")
+    },[debouncedValue])
+
     const prev = usePrev(count);
     return(<>
         <h1>{count}</h1>
@@ -13,6 +20,12 @@ function App(){
             setCount(c => c + 1 )
         }}>Click</button>
         <div>The previous value was {prev}</div>
+
+        <input type="text" 
+        onChange={(e) =>{
+            setInputValue(e.target.value)
+        }}/>
+
         {loading ? "loading....." : JSON.stringify(response)}
     </>);
 }
